69941cb00d287ad7d5d538e9368df7d882810e70
Cloverix <168465860+Cloverix@users.noreply.github.com>
2024-05-08 19:53:09 +0300

    Add files via upload

---
+++ ForGit.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << "*\n";
+    for (int i = 0; i < n - 2; i++) {
+        std::cout << "*";
+        for (int j = 0; j < i + 1; j++) {
+            std::cout << " ";
+        }
+        std::cout << "*\n";
+    }
+    for (int i = 0; i < n + 1; i++) {
+        std::cout << "*";
+    }
+}
